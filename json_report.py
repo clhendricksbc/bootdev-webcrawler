fa235e45119317bfb3ef709ffc7e802daa61e54a
clhendricksbc <4546117+clhendricksbc@users.noreply.github.com>
2026-03-23 14:44:36 -0700

    updated json_report.py with minor additions; added json report to .gitignore

--- json_report.py
+++ json_report.py
@@ -1,10 +1,15 @@
 import json
 
 def write_json_report(page_data, filename="report.json"):
+    if not page_data:
+        print("No data to write to JSON")
+        return
+    
     pages = sorted(page_data.values(), key = lambda p: p["url"])
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(pages, f, indent=2)
 
+    print(f"Report written to {filename}")
 
 
 
